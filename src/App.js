2149dd79b9a066c20b56863e1a83b64473878980
Hubert Lemczak <hubertlemczak@gmail.com>
2022-07-14 11:54:31 +0100

    extensions complete

--- src/App.js
+++ src/App.js
@@ -1,44 +1,93 @@
-import { useState } from "react"
-import {initialWorkouts, generateWorkout} from "./Workouts.js"
-import "./App.css"
+import { useState } from 'react';
+import { initialWorkouts, generateWorkout } from './Workouts.js';
+import './App.css';
+import { useEffect } from 'react';
 
 function App() {
-  const [workouts, setWorkouts] = useState(initialWorkouts)
+  const [workouts, setWorkouts] = useState(initialWorkouts);
+  const [filteredWorkouts, setFilteredWorkouts] = useState();
+  const [showDoneWorkouts, setShowDoneWorkouts] = useState(false);
 
-  const addNewWorkout = () => {
-    const newWorkout = generateWorkout()
-    console.log("addNewWorkout:", newWorkout)
-  }
+  const addNewWorkout = () => setWorkouts([...workouts, generateWorkout()]);
 
-  const deleteWorkout = (workout) => {
-    console.log("deleteWorkout:", workout)
-  }
+  const deleteWorkout = (workout) =>
+    setWorkouts(workouts.filter((item) => item !== workout));
 
   const completeWorkout = (workout) => {
-    console.log("completeWorkout:", workout)
-  }
+    setWorkouts(
+      workouts.map((item) =>
+        item === workout ? { ...item, done: true } : item
+      )
+    );
+  };
+
+  const rerollWorkout = (workout) =>
+    setWorkouts(
+      workouts.map((item) => (item === workout ? generateWorkout() : item))
+    );
+
+  useEffect(() => {
+    const updatedFilteredWorkouts = { incomplete: [], complete: [] };
+    workouts.map((workout) =>
+      workout.done === false
+        ? updatedFilteredWorkouts.incomplete.push(workout)
+        : updatedFilteredWorkouts.complete.push(workout)
+    );
+
+    setFilteredWorkouts(updatedFilteredWorkouts);
+  }, [workouts]);
+
+  const updateShowDoneWorkouts = (e) => {
+    setShowDoneWorkouts(e.target.checked);
+  };
 
   return (
     <div className="App">
       <h1>🏋️‍♀️Workout Generator</h1>
       <button onClick={addNewWorkout}>Add New Workout</button>
+      <label htmlFor="doneWorkoutsCheckbox">Show Done Only</label>
+      <input
+        id="doneWorkoutsCheckbox"
+        type="checkbox"
+        checked={showDoneWorkouts}
+        onChange={updateShowDoneWorkouts}
+      />
       <ul>
-        {workouts.map((workout, index) => (
-          <li key={index}>
-            <p>
-              {workout.sets}x sets of <strong>{workout.reps}x{workout.exercise}</strong> with {workout.rest} seconds rest
-            </p>
-            {!workout.done && 
-              <button onClick={e=>completeWorkout(workout)}>Done</button>}
-            {workout.done && 
-             <p>✅</p>}
-            <button onClick={e=>deleteWorkout(workout)}>Delete</button>
-          </li>
-        ))}
+        {filteredWorkouts &&
+          !showDoneWorkouts &&
+          filteredWorkouts.incomplete.map((workout, index) => (
+            <li key={index}>
+              <p>
+                {workout.sets}x sets of{' '}
+                <strong>
+                  {workout.reps}x{workout.exercise}
+                </strong>{' '}
+                with {workout.rest} seconds rest
+              </p>
+              {!workout.done && (
+                <button onClick={(e) => completeWorkout(workout)}>Done</button>
+              )}
+              <button onClick={(e) => deleteWorkout(workout)}>Delete</button>
+              <button onClick={(e) => rerollWorkout(workout)}>ReRoll</button>
+            </li>
+          ))}
+        {filteredWorkouts &&
+          filteredWorkouts.complete.map((workout, index) => (
+            <li key={index}>
+              <p>
+                {workout.sets}x sets of{' '}
+                <strong>
+                  {workout.reps}x{workout.exercise}
+                </strong>{' '}
+                with {workout.rest} seconds rest
+              </p>
+              {workout.done && <p>✅</p>}
+              <button onClick={(e) => deleteWorkout(workout)}>Delete</button>
+            </li>
+          ))}
       </ul>
-      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
